ae614a409571e033fa237319b2bbb0c48d19d8b3
Megzo0 <115018017+Megzo0@users.noreply.github.com>
2024-04-23 19:10:06 +0200

    Delete Hisham_project(بعد تعديل ميجز) directory

--- Hisham_project(بعد تعديل ميجز)/megzo/registeration.js
+++
@@ -1,9 +0,0 @@
-form.addEventListener("submit",(e)=>{
-    e.preventDefault();
-    if(username.value == "" || email.value == "" || password.value == "" || conPassword.value == ""){
-        alert("Please fill all the fields");
-    }
-    else if(password.value!= conPassword.value){
-        alert("Passwords do not match");
-    }
-});
